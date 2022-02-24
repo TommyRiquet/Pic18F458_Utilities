4e51166aa0a7029b0f288bc054550a164af15dc2
Neøw <71312500+TommyRiquet@users.noreply.github.com>
2022-02-24 20:42:56 +0100

    Update InterfacePython.py

--- InterfacePython.py
+++ InterfacePython.py
@@ -1,23 +1,60 @@
-import time
 import serial
-import tkinter
+from tkinter import *
 
 Port = 'COM2'
 Rate = 9600
 
-# Liaison avec le PIC
+
+def getVal():
+    Val = (serial.readline()).decode()
+    print(Val)
+
+
+def sendVal():
+    try:
+        int(inputVal.get())  # Test si valeur correcte
+        labelErrIn.config(fg='black')
+        ErrIn.set('')
+
+        print('Valeur envoyé :', inputVal.get())
+        ValOut.set(inputVal.get())  # A MODIF pour écrire sur le port !!!!!!
+    except ValueError:
+        ErrIn.set('Erreur')
+        labelErrIn.config(fg='red')
+
+
+"""
+Interface Graphique
+"""
+window = Tk()
+window.title('PIC18F458')
+window.geometry('200x200')
+window.resizable(False, False)
+
+frameIn = Frame(window)
+labelValIn = Label(frameIn, text='Valeur', font=('Arial', 12), fg='black')
+labelValIn.pack()
+inputVal = Entry(frameIn, width=9, textvariable=IntVar())
+inputVal.pack()
+ErrIn = StringVar()
+labelErrIn = Label(frameIn, textvariable=ErrIn, font=('Arial', 15), fg='black')
+labelErrIn.pack()
+btnSend = Button(frameIn, text="Send", font=('Arial', 12), padx=4, pady=4, height=1, width=5, command=sendVal)
+btnSend.pack()
+frameIn.pack()
+
+frameOut = Frame(window)
+ValOut = StringVar()
+labelValOut = Label(frameOut, textvariable=ValOut, font=('Arial', 15), fg='black')
+labelValOut.pack()
+frameOut.pack()
+
+window.mainloop()
+
+"""
+Liaison avec le PIC
+"""
+
 serial = serial.Serial(Port, Rate)
-
-
-def read():
-    line = (serial.readline()).decode()
-    print(line)
-
-
-def write(x):
-    serial.write(x)
-
-
-write(b'1')
 
 serial.close()
