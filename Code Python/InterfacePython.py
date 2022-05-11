597c6a8a0d2e59cf7da1db6b3ac4b29f1e59cc4e
QuentinLaruelle <71380161+QuentinLaruelle@users.noreply.github.com>
2022-05-11 17:18:52 +0200

    Interface Python

--- Code Python/InterfacePython.py
+++ Code Python/InterfacePython.py
@@ -2,48 +2,33 @@ import serial
 from tkinter import *
 import time
 
-Port = 'COM2'
+Port = 'COM3'
 Rate = 9600
-
-good = 'Good'
-alerte = 'Alerte'
 
 portSerie = serial.Serial(Port, Rate)
 
 
 def getVal():
-    Val = (portSerie.readline()).decode()
-    print(Val)
-    Val = Val[:-1]
-    if Val == good:
-        ValOut.set("GOOD")
-        labelValIn.config(fg='green')  # Si Alarme
-    elif Val == alerte:
-        ValOut.set('NOT GOOD')
-        labelValIn.config(fg='red')  # Si pas d'alarme
-    else:
-        print("Distance actuelle :" + Val + 'cm')  # Si rien
-        ValOut.set(Val + ' cm')
-    portSerie.flush()
+    while True :
+        Val = portSerie.readline().decode()
+        Val = Val[:-1]
+        ValOut.set(Val + " cm")
+        if int(Val) < 100:
+            labelValIn.config(fg='green')  # Si Alarme
+        else:
+            labelValIn.config(fg='red')  # Si pas d'alarme
+
+        window.update()
+
+        portSerie.flush()
 
 
-def sendVal():
-    try:
-        int(inputVal.get())  # Test si valeur correcte
-        labelErrIn.config(fg='black')
-        ErrIn.set(inputVal.get())
-
-        # portSerie.write(bytes(':'+'1036' % int(inputVal.get()), 'utf-8'))
-
-        print('Valeur envoyé :', inputVal.get())
-    except ValueError:
-        ErrIn.set('Erreur')
-        labelErrIn.config(fg='red')
 
 
 """
 Interface Graphique
 """
+
 window = Tk()
 window.title('PIC18F458')
 window.geometry('200x200')
@@ -52,13 +37,6 @@ window.resizable(False, False)
 frameIn = Frame(window)
 labelValIn = Label(frameIn, text='Valeur', font=('Arial', 12), fg='black')
 labelValIn.pack()
-inputVal = Entry(frameIn, width=9, textvariable=IntVar())
-inputVal.pack()
-ErrIn = StringVar()
-labelErrIn = Label(frameIn, textvariable=ErrIn, font=('Arial', 15), fg='black')
-labelErrIn.pack()
-btnSend = Button(frameIn, text="Send", font=('Arial', 12), padx=4, pady=4, height=1, width=5, command=sendVal)
-btnSend.pack()
 frameIn.pack()
 
 frameOut = Frame(window)
@@ -67,10 +45,7 @@ labelValOut = Label(frameOut, textvariable=ValOut, font=('Arial', 15), fg='black
 labelValOut.pack()
 frameOut.pack()
 
-window.mainloop()
 
-"""
-Liaison avec le PIC
-"""
+getVal()
 
 portSerie.close()
