955d97fbf1119d6830ae364dc7ff23f38363e72b
QuentinLaruelle <71380161+QuentinLaruelle@users.noreply.github.com>
2022-05-16 12:53:10 +0200

    Add files via upload

--- Code C/main.c
+++ Code C/main.c
@@ -1,11 +1,15 @@
 #include <main.h>
+#include <stdio.h>
+#use rs232(baud=9600, xmit=PIN_C6, rcv=PIN_C7, ERRORS)
 
 int16 dizaine,unite;
 int16 limit_distance = 100;
 int16 distance,time;
 
+
 void main()
 {
+   
    setup_low_volt_detect(FALSE);
    setup_adc_ports(NO_ANALOGS);
    setup_adc(ADC_OFF);
@@ -17,68 +21,65 @@ void main()
    setup_comparator(NC_NC_NC_NC);
    setup_vref(FALSE);
    setup_oscillator(False);
-   
+    
    set_timer1(0);
-
+   
    while(TRUE)
    {
-   
         //////////////////////////////
         //          DATA            //
         //////////////////////////////
         
-        output_high(PIN_C1);
-        delay_us(30);
-        output_low(PIN_C1);
-        
-        while(!input(PIN_C0)){}
-        
-        set_timer1(0);
-        
-        while(input(PIN_C0)){}
-        
-        time = get_timer1();
-        distance = time;
-        delay_ms(500);
-        
-        
+  output_high(PIN_C0);
+  delay_us(30);
+  output_low(PIN_C0);
+  
+  while(!input(PIN_C1)){}
+  set_timer1(0);
+  while(input(PIN_C1)){}
+  
+  
+  time = get_timer1();
+  distance = time*0.025;
+  
+  
+  delay_ms(500);
+  
         //////////////////////////////
         //          LEDS            //
         //////////////////////////////
         
-        
-        if(distance < limit_distance){    //LED Verte 
-           output_high(PIN_E0);
-           output_low(PIN_E1);
-           //printf("Good");
-        }else{                            //LED Rouge
-           output_toggle(PIN_E1);
-           output_low(PIN_E0);
-           //printf("Alerte");
-        }
-        
-        
+  if(distance < limit_distance){ 
+     output_high(PIN_E1);
+     output_low(PIN_E0);
+     
+  }else{
+     output_toggle(PIN_E0);
+     output_low(PIN_E1);
+  }
+  printf("%ld\n",distance);
+ 
+  
         //////////////////////////////
         //      AFFICHEURS 7Seg     //
         //////////////////////////////
-        if(distance < 100){               //Distance en centimètre
-           output_low(PIN_E2);
-           
-           dizaine = distance /10;
-           unite = distance - (dizaine*10);
-           output_b((dizaine<<4)+unite);
-        }else{                            //Distance en mètre
-            output_high(PIN_E2);
-            
-            dizaine = distance / 100;
-            unite = (distance - (dizaine*100))/10;
-            output_b((dizaine<<4)+unite);
-        }
+        
+  if(distance < 100){
+     output_low(PIN_E2);
+     distance = distance ;
+     dizaine = distance /10;
+     unite = distance - (dizaine*10);
+     output_b((dizaine<<4)+unite);
+  }else{
+      output_high(PIN_E2);
+      
+      dizaine = distance / 100;
+      unite = (distance - (dizaine*100))/10;
+      
+  }
   
-   }
-
 }
-
+   }
 //////////////////////////////////////////////////
 //                ||                            //
 //    PIN C0      ||    Pin Trigger de la sonde //
@@ -89,5 +90,3 @@ void main()
 //    PIN B0-7    ||    8 Pins des afficheurs   //
 //                ||                            //
 //////////////////////////////////////////////////
-
-
